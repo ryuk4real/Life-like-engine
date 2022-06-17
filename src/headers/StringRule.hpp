c6ec62fe66d32225f579e8792ecc2343e3f00b68
Pasquale Tudda <84407165+ryuk4real@users.noreply.github.com>
2022-06-17 23:33:44 +0200

    fixed ragex and subsets of the rulestring parsing

--- src/headers/StringRule.hpp
+++ src/headers/StringRule.hpp
@@ -32,7 +32,7 @@ class StringRule
 StringRule::StringRule(string &_stringRule)
 {
     
-    const regex ruleDetection("^B((?!\\d*?(\\d)\\d*?\\2)\\d{0,10})\\/S((?!\\d*?(\\d)\\d*?\\4)\\d{0,10})");
+    const regex ruleDetection("^B((?![0-8]*?([0-8])[0-8]*?\\2)[0-8]{0,10})\\/S((?![0-8]*?([0-8])[0-8]*?\\4)[0-8]{0,10})");
     smatch matches;
     
     if (regex_search(_stringRule, matches, ruleDetection))
@@ -40,7 +40,7 @@ StringRule::StringRule(string &_stringRule)
         this->stringRule = _stringRule;
 
 
-        this->bornSubset = vector<int>(10, 0);
+        this->bornSubset = vector<int>(9, 0);
         string bornSubString = matches.str(1);
 
         for (int i = 0; i < bornSubString.size(); ++i)
@@ -49,18 +49,17 @@ StringRule::StringRule(string &_stringRule)
         }
 
 
-
-
+        this->survivorSubset = vector<int>(9, 0);
         string survivorSubString = matches.str(3);
-        this->survivorSubset = vector<int>(10, 0);
 
         for (int i = 0; i < survivorSubString.size(); ++i)
         {
             survivorSubset[survivorSubString[i] - 48] = 1;
         }
 
-
+        cout<<endl;
     }
+    
     else throw runtime_error("ERROR: can't parse stringRule in file settings.json");
 }
 
