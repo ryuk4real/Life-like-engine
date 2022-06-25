94641ce3409ba894c9efb1737f4adbddd4792885
Pasquale Tudda <84407165+ryuk4real@users.noreply.github.com>
2022-06-26 01:36:06 +0200

    main refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,40 @@
 #include <mpi.h>
 
+#include <random>
+#include <thread> //sleep_for
+#include <chrono> //milliseconds
+
+
 #include "headers/serialLifeEngine.cpp"
 #include "headers/parallelLifeEngine.cpp"
 
 int main(int args, char **argv)
 {
+
     MPI_Init(&args, &argv);
+
 
     int numberOfCpus;
     MPI_Comm_size(MPI_COMM_WORLD, &numberOfCpus);
 
+
+    int errorCode;
+
+
     if (numberOfCpus == 1)
     {
-        int errorCode;
         errorCode = serialLifeEngine();
 
         MPI_Finalize();
         return errorCode;
     }
-    else if (numberOfCpus == 2 || numberOfCpus % 2 == 1)
+    else if (numberOfCpus >= 2)
     {
-        parallelLifeEngine(numberOfCpus);
+
+        errorCode = parallelLifeEngine(numberOfCpus);
+
+        MPI_Finalize();
+        return errorCode;
     }
 
-
-
-
-    return 0;
 }
